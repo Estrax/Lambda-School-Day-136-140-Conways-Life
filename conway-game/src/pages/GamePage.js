d9aead60fc850ff98e497b05df51b5ef2d627607
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 17:11:37 -0400

    added game page

---
+++ conway-game/src/pages/GamePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class GamePage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>GamePage</h1>
+			</div>
+		);
+	}
+}
+
+GamePage.propTypes = {};
+
+export default GamePage;
