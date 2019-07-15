b5c353601f8b671573863f53cbb7d89374196367
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 17:09:13 -0400

    added login, registration and game routes

--- conway-game/src/routes/index.js
+++ conway-game/src/routes/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 
-// import { RouteAuthNeeded, RouteNoAuthNeeded } from '../hoc';
-import { HomePage } from '../pages';
+import { RouteAuthNeeded, RouteNoAuthNeeded } from '../hoc';
+import { HomePage, LoginPage, RegisterPage, GamePage } from '../pages';
 
 const Routes = (props) => {
 	return (
 		<Router history={props.history}>
-			<Route exact path='/' component={HomePage} />
+			<Route exact path='/' component={RouteNoAuthNeeded(HomePage)} />
+			<Route
+				exact
+				path='/login'
+				component={RouteNoAuthNeeded(LoginPage)}
+			/>
+			<Route
+				exact
+				path='/register'
+				component={RouteNoAuthNeeded(RegisterPage)}
+			/>
+			<Route exact path='/game' component={RouteAuthNeeded(GamePage)} />
 		</Router>
 	);
 };
