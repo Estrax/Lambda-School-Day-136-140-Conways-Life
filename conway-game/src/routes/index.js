13bdc827e9cf7f500b29e7b6a6eabe11b58ca919
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 17:20:03 -0400

    added navbar to routes

--- conway-game/src/routes/index.js
+++ conway-game/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
+import { Navbar } from '../containers';
 
 import { RouteAuthNeeded, RouteNoAuthNeeded } from '../hoc';
 import { HomePage, LoginPage, RegisterPage, GamePage } from '../pages';
@@ -7,6 +8,7 @@ import { HomePage, LoginPage, RegisterPage, GamePage } from '../pages';
 const Routes = (props) => {
 	return (
 		<Router history={props.history}>
+			<Navbar />
 			<Route exact path='/' component={RouteNoAuthNeeded(HomePage)} />
 			<Route
 				exact
