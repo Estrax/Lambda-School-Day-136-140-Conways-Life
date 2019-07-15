d6196a533970e0ff457d351b1d729afbeb810605
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 16:32:50 -0400

    added redux store

--- conway-game/src/store/index.js
+++ conway-game/src/store/index.js
@@ -1,1 +1,6 @@
-export default {};
+import { createStore, applyMiddleware } from 'redux';
+import reducers from '../reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+export default createStore(reducers, applyMiddleware(thunk, logger));
