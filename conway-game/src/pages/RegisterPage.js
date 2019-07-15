daeff2d9821e574d8a0544c9bc56f25f8f18b65a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 17:11:49 -0400

    added register page

---
+++ conway-game/src/pages/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RegisterPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>RegisterPage</h1>
+			</div>
+		);
+	}
+}
+
+RegisterPage.propTypes = {};
+
+export default RegisterPage;
