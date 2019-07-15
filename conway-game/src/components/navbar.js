1e6a271c6a03cd81d77f087ac9c5989a3bc49d9a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 17:19:47 -0400

    added navbar component

---
+++ conway-game/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Navbar extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Navbar</h1>
+			</div>
+		);
+	}
+}
+
+Navbar.propTypes = {};
+
+export default Navbar;
