bb96e1399b646a84252f4c0f21faa460a3be05ca
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-15 16:55:46 -0400

    added routes - main route

--- conway-game/src/routes/index.js
+++ conway-game/src/routes/index.js
@@ -1,1 +1,15 @@
-export default {};
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+
+// import { RouteAuthNeeded, RouteNoAuthNeeded } from '../hoc';
+import { HomePage } from '../pages';
+
+const Routes = (props) => {
+	return (
+		<Router history={props.history}>
+			<Route exact path='/' component={HomePage} />
+		</Router>
+	);
+};
+
+export default Routes;
