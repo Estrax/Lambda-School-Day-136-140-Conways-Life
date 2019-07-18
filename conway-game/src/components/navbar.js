12e4f886042d5e0f79ce2e9c33ae323a77b13192
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 15:53:31 -0400

    updated navbar - added links

--- conway-game/src/components/navbar.js
+++ conway-game/src/components/navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Navbar extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Navbar</h1>
+				<h1>Conway's Game of Life</h1>
+				<Link to='/game'>Game</Link>
+				<Link to='/about'>About</Link>
+				<Link to='/rules'>Rules</Link>
 			</div>
 		);
 	}
