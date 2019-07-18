3e05d4d4e483314bdce1984691d9f3d4c79beb6b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 15:52:13 -0400

    added rules page

---
+++ conway-game/src/pages/RulesPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RulesPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>RulesPage</h1>
+			</div>
+		);
+	}
+}
+
+RulesPage.propTypes = {};
+
+export default RulesPage;
