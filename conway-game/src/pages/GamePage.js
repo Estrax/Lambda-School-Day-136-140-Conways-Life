446a32273154640950c67e83331f28aa37d15709
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 15:54:29 -0400

    updated a game page

--- conway-game/src/pages/GamePage.js
+++ conway-game/src/pages/GamePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Game } from '../components';
 
 class GamePage extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>GamePage</h1>
+				<Game />
 			</div>
 		);
 	}
