387ca8e5b1c1e1aea606ae4ed4f6bb3bc3ab3e4a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 16:24:43 -0400

    updated the rules page - added rules

--- conway-game/src/pages/RulesPage.js
+++ conway-game/src/pages/RulesPage.js
@@ -5,7 +5,24 @@ class RulesPage extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>RulesPage</h1>
+				<h1>Rules</h1>
+				<p>
+					In the game of life, each cell can have one out of multiple
+					states available - there, it can be alive or dead.
+					<br />
+					<br />
+					Alive cells remain alive as long as they have at least{' '}
+					<b>two</b> neighbors. The cell will die due to loneliness
+					when it has less neighbors.
+					<br />
+					<br />
+					At the same time, if a cell has <b>more than three</b>{' '}
+					neighbors, it is also going to die due to the "social
+					burnout". However, the dead cell that is surrounded by{' '}
+					<b>three</b> neighbors is coming back to life.
+					<br />
+					<br />
+				</p>
 			</div>
 		);
 	}
