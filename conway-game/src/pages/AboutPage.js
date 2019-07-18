9e559da4e35b0340617e53f4620edbe06f00162a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 15:52:02 -0400

    added an about page

---
+++ conway-game/src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>AboutPage</h1>
+			</div>
+		);
+	}
+}
+
+AboutPage.propTypes = {};
+
+export default AboutPage;
