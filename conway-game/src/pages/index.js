216f4cee0c198243cd22b3488bd9a657c4a16929
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-18 15:52:43 -0400

    updated index pages

--- conway-game/src/pages/index.js
+++ conway-game/src/pages/index.js
@@ -2,5 +2,7 @@ import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
 import GamePage from './GamePage';
+import AboutPage from './AboutPage';
+import RulesPage from './RulesPage';
 
-export { HomePage, LoginPage, RegisterPage, GamePage };
+export { HomePage, LoginPage, RegisterPage, GamePage, AboutPage, RulesPage };
