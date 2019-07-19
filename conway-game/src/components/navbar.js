78eb7d28c3dc12de2bfbde6c908cef51bc864373
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-19 09:29:30 -0400

    added default bootstrap navbar

--- conway-game/src/components/navbar.js
+++ conway-game/src/components/navbar.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Navbar extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Conway's Game of Life</h1>
-				<Link to='/game'>Game</Link>
-				<Link to='/about'>About</Link>
-				<Link to='/rules'>Rules</Link>
-			</div>
+			<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+				<Link className='navbar-brand' to='/'>
+					Conway's Game of Life
+				</Link>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbar'
+					aria-controls='navbar'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon' />
+				</button>
+
+				<div className='collapse navbar-collapse' id='navbar'>
+					<ul className='navbar-nav ml-auto'>
+						<li className='nav-item'>
+							<Link to='/game' className='nav-link'>
+								Game
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link to='/about' className='nav-link'>
+								About
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link to='/rules' className='nav-link'>
+								Rules
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		);
 	}
 }
